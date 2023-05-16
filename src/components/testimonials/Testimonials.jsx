554be73a5e8d64faa740b792465c81ee58f8d76a
Victor Perdiguer <victor.perdiguer@gmail.com>
2023-05-16 09:48:48 +0200

    testimonials done

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import './Testimonials.css';
 import TestimonialCard from "./TestimonialCard";
 import data from "./data";
+import { Navigation, Pagination, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 
 const Testimonials = (props) => {
@@ -10,19 +14,28 @@ const Testimonials = (props) => {
     <section id="testimonials">
       <h5>What people have to say about me</h5>
       <h2>Testimonials</h2>
-      <div className="container testimonials__container">
-        {data.map((testimonial, index) => 
-          <TestimonialCard
-            key={testimonial.index}
-            picture={testimonial.picture}
-            link={testimonial.link}
-            name={testimonial.name}
-            subtitle={testimonial.subtitle}
-            review={testimonial.review}
-            />
+      <Swiper 
+        className="container testimonials__container"
+        modules={[Pagination]}
+        spaceBetween={50}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        {data.map((testimonial, index) => {
+          return (
+            <SwiperSlide>
+              <TestimonialCard
+              key={index}
+              picture={testimonial.picture}
+              link={testimonial.link}
+              name={testimonial.name}
+              subtitle={testimonial.subtitle}
+              review={testimonial.review}
+              />
+            </SwiperSlide>
           )
-        }    
-      </div>
+        })}    
+      </Swiper>
     </section>
   )
 };
